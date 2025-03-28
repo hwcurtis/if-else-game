4d80b07e376bb2703b06a3c9b1ba9cc0d45c9929
hwcurtis <hopewcurtis@gmail.com>
2025-03-27 23:01:57 -0400

    game

---
+++ if else version1 app.py
@@ -0,0 +1,9 @@
+
+
+user_input = ("you are facing a way out, one way is a path towards the unknown, but before you procedeed, you must choose a combination")
+
+print("You are lost walking in the dark , unaware what has brought you out so far away from everyone (or so you think) on your travle you , you trip over
+an itme on the ground, wihtout hesitation you  " ) 
+choice = input ("pick the object up , jump up and dust yourself off, avoid it all togther and just run") 
+
+if choice = "you run as fast as you can away" git 
